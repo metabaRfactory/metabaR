5971ca9fcba15356c1bfb1e3b2549b207920b531
lzinger <lucie@zinger.fr>
2019-04-25 00:47:55 +0200

    modified ggpcrplate to use a metabarlist

--- R/m_ggpcrplate.R
+++ R/m_ggpcrplate.R
@@ -1,59 +1,85 @@
-#' Plotting an amplicon attribute in the PCR plate design
+#' Plotting a PCR attribute in the PCR plate design
 #'
-#' Plots an attribute of amplicons from a \code{\link{TODEFINE}} object in the correspondint 96-well PCR plate design
+#' Plots an attribute of PCRs from a \code{\link{metabarlist}} object in the correspondint 96-well PCR plates design
 #'
 #'
-#' @param attr    a numeric vector of the amplicon's attribute to be plotted
-#' @param plate_no  a numeric vector of the plate id(s) where the amplicons were amplified
-#' @param plate_col  a numeric vector of the plate's column where the amplicons were amplified
-#' @param plate_row  a character vector of letters corresponding to the plate's row where the amplicons were amplified
-#' @param control_type  a factor indicating the amplicon type (controls type, sample types, etc.)
+#' @param metabarlist    a \code{\link{metabarlist}} object
+#' @param table          the table where the information on which the plotting is based. Can be only `reads`,  or `pcrs`.
+#' @param index          the name of the column name containing the information to be plotted. This information should be a numeric vector.
 #'
 #' @name ggpcrplate
 #'
 #' @return a ggplot
 #'
 #' @details
-#' Vizualizing some attributes of the amplicon in its experimental context, here its location in one or a set of 96-well PCR plates, can be useful to identify potential problems (e.g. low amount of reads due to unfunctional primer set).
+#' Vizualizing some attributes of the PCRs in their plate design context, i.e. their location in one or a set of 96-well PCR plates. This can be useful to identify potential problems (e.g. low amount of reads due to non-functional primer sets).
 #'
 #' @examples
 #'
 #' data(soil_euk)
-#' #plot number of reads per pcrs
-#' seqdepth = rowSums(soil_euk$reads)
-#' p = ggpcrplate(attr = seqdepth, plate_no = soil_euk$pcrs$plate_no,
-#'                   plate_col = soil_euk$pcrs$plate_col,
-#'                   plate_row =  soil_euk$pcrs$plate_row,
-#'                   control_type = soil_euk$pcrs$Control_type)
-#' p + labs(size="# reads", fill="control type") +
-#'   scale_fill_manual(values=c("brown", "pink", "cyan4", "red"),
-#'                     na.value = "white")
+#'
+#' #Plot the number of reads per pcrs
+#'
+#' ##Store the number of reads per pcrs in the pcrs table
+#' soil_euk$pcrs$seq_depth = rowSums(soil_euk$reads)
+#'
+#' ##Plot the results
+#' ggpcrplate(soil_euk, table = "pcrs", index = "seq_depth")
+#'
+#'
+#' #Plot the number of reads of the most abundant MOTU
+#'
+#' ##Get the name of the most abundant MOTU
+#' id = colnames(soil_euk$reads)[which.max(colSums(soil_euk$reads))]
+#'
+#' ##Plot the results
+#' ggpcrplate(soil_euk, table = "reads", index = id) + labs(size="#reads of most\nabundant MOTU")
+#'
 #'
 #' @author Lucie Zinger
 #' @import ggplot2
 #' @export ggpcrplate
 
+ggpcrplate = function(metabarlist, table, index) {
 
-ggpcrplate = function(attr, plate_no, plate_col, plate_row, control_type) {
+  if(suppressWarnings(check_metabarlist(metabarlist))) {
 
-  if (is.numeric(attr)==F)
-    stop("attr should be numeric")
-  if (length(grep("FALSE", as.numeric(plate_col) %in% 1:12)))
-    stop("plate_col should range between 1 and 12")
-  if (length(grep("FALSE", plate_row %in% LETTERS[1:8])))
-    stop("plate_row should range between A and H")
+    cols_plate_design = c('plate_no', 'plate_col', 'plate_row')
 
-  dat = data.frame(plate_no = plate_no, plate_col = as.numeric(plate_col),
-                   plate_row = plate_row, attr=attr, control_type = control_type)
+    if (!all(cols_plate_design %in% colnames(metabarlist$pcrs)))
+      stop("PCR plate design not properly provided: ",
+           paste(cols_plate_design[!cols_plate_design %in% colnames(metabarlist$pcrs)], sep=', '),
+           " missing !\n")
 
-  ggplot(dat, aes(y=match(plate_row, LETTERS[1:8]), x=plate_col, size=attr)) +
-    geom_raster(aes(fill=control_type)) +
-    facet_wrap(~paste("Plate", plate_no), scale="free") + theme_bw() +
-    scale_y_reverse(breaks = 1:8, labels=LETTERS[1:8]) +
-    scale_x_continuous(breaks = 1:12) +
-    geom_point() +
-    labs(x=NULL, y=NULL)
+    extract_table_methods = c("reads", "pcrs")
+    tab = match.arg(table, extract_table_methods)
+
+    if(!index %in% colnames(metabarlist[[tab]]))
+      stop("index is not in table")
+
+    if(!is.numeric(metabarlist[[tab]][,index]))
+      stop("selected information should be numeric")
+
+    plate_design = metabarlist$pcrs[,c("plate_no", "plate_col", "plate_row", "control_type")]
+    plate_design$control_type = factor(plate_design$control_type,
+                                       levels=c("extraction", "pcr", "sequencing", "positive", NA))
+
+    plate_design[index] = metabarlist[[tab]][,index]
+
+    plate_design[index][plate_design[index]==0] = NA
+
+    ggplot(plate_design, aes(y=match(plate_row, LETTERS[1:8]), x=plate_col, size=get(index))) +
+      geom_raster(aes(fill=control_type)) +
+      facet_wrap(~plate_no, scale="free") + theme_bw() +
+      scale_y_reverse(breaks = 1:8, labels=LETTERS[1:8]) +
+      scale_x_continuous(breaks = 1:12) +
+      scale_fill_manual(values=c("brown", "red", "pink", "cyan4", "white")) +
+      geom_point() +
+      labs(x=NULL, y=NULL, size=index)
+
+  }
 }
+
 
 
 
