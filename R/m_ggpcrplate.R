8a5fdf279e76f673544b5a423493ff6c488e7613
Clément Lionnet <clement.lionnet@univ-grenoble-alpes.fr>
2019-05-10 16:14:56 +0200

    format style and remove unused line

--- R/m_ggpcrplate.R
+++ R/m_ggpcrplate.R
@@ -24,8 +24,8 @@
 #'
 #' #Plot the number of reads of the most abundant MOTU
 #' library(ggplot2)
-#' ggpcrplate(soil_euk, FUN=function(m){m$reads[,which.max(colSums(m$reads))]})) +
-#' labs(size="#reads of most\nabundant MOTU")
+#' ggpcrplate(soil_euk, legend_title = "#reads of most\nabundant MOTU",
+#' FUN = function(m){m$reads[,which.max(colSums(m$reads))]}))
 #'
 #'
 #' @author Lucie Zinger
@@ -33,7 +33,8 @@
 #' @export ggpcrplate
 
 
-ggpcrplate = function(metabarlist, legend_title="well_values", FUN = function(metabarlist) {rowSums(metabarlist$reads)}) {
+ggpcrplate = function(metabarlist, legend_title = "well_values",
+                      FUN = function(metabarlist) {rowSums(metabarlist$reads)}) {
 
   if(suppressWarnings(check_metabarlist(metabarlist))) {
 
@@ -50,26 +51,23 @@ ggpcrplate = function(metabarlist, legend_title="well_values", FUN = function(me
 
     if (!all(cols_plate_design %in% colnames(metabarlist$pcrs)))
       stop("PCR plate design not properly provided: ",
-           paste(cols_plate_design[!cols_plate_design %in% colnames(metabarlist$pcrs)], sep=', '),
+           paste(cols_plate_design[!cols_plate_design %in% colnames(metabarlist$pcrs)], sep = ', '),
            " missing !\n")
 
-    plate_design <- metabarlist$pcrs[,c("plate_no", "plate_col", "plate_row", "control_type")]
-    plate_design_levels <- c(levels(plate_design$control_type), "sample")
-    plate_design$control_type <- factor(plate_design$control_type, levels=plate_design_levels)
-    plate_design$control_type[is.na(plate_design$control_type)] <- "sample"
-    plate_design$control_type <- factor(plate_design$control_type,
-                                       levels=c("extraction", "pcr", "sequencing", "positive", "sample"))
+    plate_design <- metabarlist$pcrs[, c("plate_no", "plate_col", "plate_row", "control_type")]
+    plate_design_levels <- c("extraction", "pcr", "sequencing", "positive")
+    plate_design$control_type <- factor(plate_design$control_type, levels = plate_design_levels)
 
     plate_design$well_values <- function_values
-    plate_design$well_values[plate_design$well_values==0] <- NA
+    plate_design$well_values[plate_design$well_values == 0] <- NA
 
-    ggplot(plate_design, aes(y=match(plate_row, LETTERS[1:8]), x=plate_col, size=well_values)) +
-      geom_raster(aes(fill=control_type)) +
-      facet_wrap(~plate_no, scale="free") + theme_bw() +
-      scale_y_reverse(breaks = 1:8, labels=LETTERS[1:8]) +
+    ggplot(plate_design, aes(y = match(plate_row, LETTERS[1:8]), x = plate_col, size = well_values)) +
+      geom_raster(aes(fill = control_type), na.rm = TRUE) +
+      facet_wrap(~plate_no, scale = "free") + theme_bw() +
+      scale_y_reverse(breaks = 1:8, labels = LETTERS[1:8]) +
       scale_x_continuous(breaks = 1:12) +
-      scale_fill_manual(values=c("brown", "red", "pink", "cyan4", "white")) +
-      geom_point(na.rm=TRUE) +
-      labs(x=NULL, y=NULL, size=legend_title)
+      scale_fill_manual(values = c("brown", "red", "pink", "cyan4"), na.translate = FALSE) +
+      geom_point(na.rm = TRUE) +
+      labs(x = NULL, y = NULL, size = legend_title)
   }
 }
