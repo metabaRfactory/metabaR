d67d870bb91f60ee68ce0bc3ec23dc2d00b7c6ce
Clément Lionnet <clement.lionnet@univ-grenoble-alpes.fr>
2019-05-27 17:57:01 +0200

    update code to remove warning and change the parameter like in the function ggpcrplate

--- R/m_ggpcrtags.R
+++ R/m_ggpcrtags.R
@@ -4,8 +4,8 @@
 #'
 #'
 #' @param metabarlist    a \code{\link{metabarlist}} object
-#' @param table          the table where the information on which the plotting is based. Can be only `reads`,  or `pcrs`.
-#' @param index          the name of the column name containing the information to be plotted. This information should be a numeric vector.
+#'  @param FUN            a function which return a vector containing the information to be plotted. The vector should be a numeric vector which has the same length of table 'reads'.
+#' @param legend_title   the title of legend containing the plotted information.
 #' @param taglist        a character vector corresponding to the full list of tags ordered as they are used in the PCR plate scheme
 #'
 #' @name ggpcrtag
@@ -19,40 +19,32 @@
 #'
 #' data(soil_euk)
 #'
-#' taglist = c("acacacac", "acagcaca", "gtgtacat", "tatgtcag", "tagtcgca",
-#'             "tactatac", "actagatc", "gatcgcga", "cgctctcg", "gtcgtaga",
-#'             "gtcacgtc", "gactgatg", "agactatg", "gcgtcagc", "tgacatca",
-#'             "acatgtgt", "gtacgact", "atgatcgc", "acgacgag", "catcagtc",
-#'             "atcagtca", "tctactga", "gatgatct", "ctgcgtac", "agcgacta",
-#'             "tcagtgtc", "actctgct", "atatagcg", "ctatgcta", "tcgcgctg",
-#'             "agcacagt", "tagctagt", "agtgctac", "cgtataca", "cgagtcgt",
-#'             "cacatgat")
-#'
 #' #Plot the number of reads per pcrs
-#'
-#' ##Store the number of reads per pcrs in the pcrs table
-#' soil_euk$pcrs$seq_depth = rowSums(soil_euk$reads)
-#'
-#' ##Plot the results
-#' ggpcrtag(soil_euk, table = "pcrs", index = "seq_depth", taglist = taglist)
-#'
+#' ggpcrtag(soil_euk)
 #'
 #' #Plot the number of reads of the most abundant MOTU
-#'
-#' ##Get the name of the most abundant MOTU
-#' id = colnames(soil_euk$reads)[which.max(colSums(soil_euk$reads))]
-#'
-#' ##Plot the results
-#' ggpcrtag(soil_euk, table = "reads", index = id, taglist = taglist)
+#' ggpcrtag(soil_euk, legend_title = "#reads of most\nabundant MOTU",
+#'            FUN = function(m){m$reads[,which.max(colSums(m$reads))]})
 #'
 #'
 #' @author Lucie Zinger
 #' @importFrom cowplot insert_xaxis_grob insert_yaxis_grob ggdraw
 #' @export ggpcrtag
 
-ggpcrtag = function(metabarlist, table, index, taglist) {
+ggpcrtag = function(metabarlist, legend_title = "well_values",
+                    FUN = function(metabarlist) {rowSums(metabarlist$reads)},
+                    taglist=as.character(unique(soil_euk$pcrs$tag_rev))) {
 
   if(suppressWarnings(check_metabarlist(metabarlist))) {
+
+    function_values <- FUN(metabarlist)
+
+    if (length(function_values) != nrow(metabarlist$pcrs)) {
+      stop('provided information should have the length of pcrs')
+    }
+
+    if(!is.numeric(function_values))
+      stop("selected information should be numeric")
 
     cols_tag_design = c('tag_fwd', 'tag_rev')
 
@@ -61,22 +53,13 @@ ggpcrtag = function(metabarlist, table, index, taglist) {
            paste(cols_plate_design[!cols_plate_design %in% colnames(metabarlist$pcrs)], sep=', '),
            " missing !\n")
 
-    extract_table_methods = c("reads", "pcrs")
-    tab = match.arg(table, extract_table_methods)
-
-    if(!index %in% colnames(metabarlist[[tab]]))
-      stop("index is not in table")
-
-    if(!is.numeric(metabarlist[[tab]][,index]))
-      stop("selected information should be numeric")
-
     tag_design = metabarlist$pcrs[,c("tag_fwd", "tag_rev", "control_type")]
     tag_design$control_type = factor(tag_design$control_type,
-                                       levels=c("extraction", "pcr", "sequencing", "positive", NA))
+                                       levels=c("extraction", "pcr", "sequencing", "positive"))
 
-    tag_design[index] = metabarlist[[tab]][,index]
-
-    tag_design[index][tag_design[index]==0] = NA
+    # add values of parameter's function to the fictive dataframe
+    tag_design$well_values <- function_values
+    tag_design$well_values[tag_design$well_values == 0] <- NA
 
     tag_design$tag_fwd = factor(tag_design$tag_fwd,
                                 levels = rev(levels(tag_design$tag_fwd)[
@@ -87,38 +70,38 @@ ggpcrtag = function(metabarlist, table, index, taglist) {
                                   match(taglist, levels(tag_design$tag_rev), nomatch=0)])
 
    all =
-     ggplot(tag_design, aes(y=tag_fwd, x=tag_rev, size=get(index))) +
-      geom_raster(aes(fill=control_type)) +
+     ggplot(tag_design, aes(y=tag_fwd, x=tag_rev, size=well_values)) +
+      geom_raster(aes(fill=control_type), na.rm = TRUE) +
       theme_bw() +
-      scale_fill_manual(values=c("brown", "red", "pink", "cyan4", "white")) +
-      geom_point() +
-      labs(size=index) +
+      scale_fill_manual(values=c("brown", "red", "pink", "cyan4"), na.translate = FALSE) +
+      geom_point(na.rm = TRUE) +
+      labs(size=legend_title) +
       scale_x_discrete(expand=c(0.02,0)) +
       scale_y_discrete(expand=c(0.02,0)) +
       theme(axis.text.x = element_text(angle=45, h=1),
             legend.position = "bottom",
             legend.box = "vertical")
+
    fwd =
-     ggplot(tag_design, aes(x=tag_fwd, y=get(index))) +
-       geom_boxplot(lwd=0.5) +
+     ggplot(tag_design, aes(x=tag_fwd, y=well_values)) +
+       geom_boxplot(lwd=0.5, na.rm = TRUE) +
        theme_bw() +
-       stat_summary(fun.y=median, geom="line", aes(group=1), color="cyan2", lwd=1) +
-       labs(y=index, x=NULL) +
+       stat_summary(fun.y=median, geom="line", aes(group=1), color="cyan2", lwd=1, na.rm = TRUE) +
+       labs(y=legend_title, x=NULL) +
        scale_x_discrete(expand=c(0.02,0)) +
        scale_y_discrete(expand=c(0.02,0)) +
        coord_flip() +
        theme(axis.text.y = element_blank())
 
    rev =
-     ggplot(tag_design, aes(x=tag_rev, y=get(index))) +
-       geom_boxplot(lwd=0.5) +
+     ggplot(tag_design, aes(x=tag_rev, y=well_values)) +
+       geom_boxplot(lwd=0.5, na.rm = TRUE) +
        theme_bw() +
-       stat_summary(fun.y=median, geom="line", aes(group=1), color="cyan2", lwd=1) +
-       labs(y=index, x=NULL) +
+       stat_summary(fun.y=median, geom="line", aes(group=1), color="cyan2", lwd=1, na.rm = TRUE) +
+       labs(y=legend_title, x=NULL) +
        scale_x_discrete(expand=c(0.02,0)) +
        scale_y_discrete(expand=c(0.02,0)) +
        theme(axis.text.x = element_blank())
-
 
    out = insert_xaxis_grob(all, rev, grid::unit(.2, "null"), position = "top")
    out = insert_yaxis_grob(out, fwd, grid::unit(.2, "null"), position = "right")
