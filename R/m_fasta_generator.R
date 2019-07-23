52642539eecfb46d860f7622c4497a9ca105e31d
Clément Lionnet <clement.lionnet@univ-grenoble-alpes.fr>
2019-07-23 16:59:30 +0200

    update code of fasta_generator to use metabarlist object

--- R/m_fasta_generator.R
+++ R/m_fasta_generator.R
@@ -21,36 +21,83 @@
 #' # export in fasta the 10 most abundant MOTUs
 #' idx <- order(soil_euk$motus$count, decreasing = T)[1:10]
 #' fasta_generator(
-#'   "Dominants.fasta", rownames(soil_euk$motus)[idx],
-#'   soil_euk$motus$sequence[idx]
+#'   soil_euk, rownames(soil_euk$motus)[idx],
+#'   "Dominants.fasta"
 #' )
 #'
 #' # export in fasta the 10 most abundant MOTUs and their abundance
 #' idx <- order(soil_euk$motus$count, decreasing = T)[1:10]
-#' headers <- paste(rownames(soil_euk$motus)[idx],
-#'   "abundance=", soil_euk$motus$count[idx],
-#'   sep = ""
+#' annotation <- data.frame(
+#'   abundance = soil_euk$motus$count[idx],
+#'   row.names = rownames(soil_euk$motus)[idx]
 #' )
 #' fasta_generator(
-#'   "Dominants.fasta", headers,
-#'   soil_euk$motus$sequence[idx]
+#'   soil_euk, rownames(soil_euk$motus)[idx],
+#'   "Dominants.fasta",
+#'   annotation
 #' )
 #' @author Lucie Zinger
 #' @export fasta_generator
 
-fasta_generator <- function(file, id, seq) {
-  if (length(id) != length(seq)) {
-    stop("id and seq should be of the same length")
-  }
+fasta_generator <- function(metabarlist, id = rownames(metabarlist$motus), output_file = NULL, annotation = NULL, annot_sep = " ") {
+  if (suppressWarnings(check_metabarlist(metabarlist))) {
+    if (is.null(output_file)) {
+      stop("The output_file is required!")
+    }
 
-  if (file.exists(file)) {
-    file.remove(file)
-  }
-  file.create(file)
+    if (!all(id %in% rownames(metabarlist$motus))) {
+      stop("All identifiers must correspond exactly to rows of metabarlist$motus")
+    }
 
-  for (i in 1:length(id)) {
-    cat(paste(">", id[i], "\n", seq[i], "\n", sep = ""),
-      file = file, append = T
-    )
+    if (!is.null(annotation)) {
+      if (!all(id %in% rownames(annotation))) {
+        stop("All identifiers must correspond exactly to rows of annotation")
+      }
+
+      header_annot <- apply(annotation, 1, function(x) {
+        paste(names(x), x, sep = "=", collapse = annot_sep)
+      })
+      header <- sapply(id, function(x) {
+        bh <- paste(">", x, sep = "")
+        paste(bh, header_annot[x], sep = annot_sep)
+      })
+    }
+    else {
+      header <- sapply(id, function(x) {
+        paste(">", x, sep = "")
+      })
+    }
+
+    if (file.exists(output_file)) {
+      file.remove(output_file)
+    }
+    file.create(output_file)
+
+    for (i in id) {
+      cat(paste(header[i], metabarlist$motus[i, "sequence"], sep = "\n"),
+        sep = "\n", file = output_file, append = T
+      )
+    }
   }
 }
+
+
+# fasta_generator <- function(id, seq, output_file = NULL) {
+#   if (is.null(output_file)) {
+#     stop("The output_file is required!")
+#   }
+#   if (length(id) != length(seq)) {
+#     stop("id and seq should be of the same length")
+#   }
+#
+#   if (file.exists(output_file)) {
+#     file.remove(output_file)
+#   }
+#   file.create(output_file)
+#
+#   for (i in 1:length(id)) {
+#     cat(paste(">", id[i], "\n", seq[i], "\n", sep = ""),
+#       file = output_file, append = T
+#     )
+#   }
+# }
