1b045c709c6779a0a384da94c7ebc1662d7cc142
lzinger <lucie@zinger.fr>
2020-07-29 18:55:21 +0200

    Checked tagjumpslayer and added a method argument: cut sets to 0 counts, substract substracts max number of leaks to all counts of a MOTU

--- R/m_tagjumpslayer.R
+++ R/m_tagjumpslayer.R
@@ -1,25 +1,36 @@
-#' Filtering potential tag jumps in metabarcoding data
+#' Filtering potential tag-jumps in metabarcoding data
 #'
 #' Removes reads of potential tagjumps in a \code{\link{metabarlist}} object.
 #'
 #'
 #' @param metabarlist a \code{\link{metabarlist}} object
-#' @param threshold   an MOTU relative abundance value below which the MOTU is considered to be absent
+#' @param threshold   an MOTU relative abundance value below
+#'                    which the MOTU is considered to be tag-jumps
+#' @param method      a string indicating the method to deal with tag-jumps.
+#'                    Default: \code{"cut"}
 #'
 #' @name tagjumpslayer
 #'
-#' @return a \code{\link{metabarlist}} object with the transformed reads count matrix
+#' @return a \code{\link{metabarlist}} object with the curated reads count matrix
 #'
 #' @details
-#' Tagjumps are an important bias that lead to the presence of potentially high numbers of false positives in DNA metabarcoding data. The origin of this bias is not well known yet, although currently suspected to be generated during the PCR enrichment process of the sequencing library preparation. Incomplete PCR amplification at this stage may lead to the formation of chimeras at priming sites, from fragments belonging to two different amplicons. The resulting fragment is therefore strictly identical to the genuine MOTU, but its tag combination is an artifact. This bias is also frequency-dependent, i.e. abundant genuine MOTUs are more likely to be found in low abundance in samples were they are not supposed to be. The function aims at reducing the amount of such false positives, by considering each MOTU separately and setting to 0 any abundance representing < 0.03% of the total MOTU abundance in the entire dataset.
+#' Tagjumps are an important bias that lead to the presence of potentially high numbers of false positives in DNA metabarcoding data. The exact mechanism generating this bias is not well characterized yet, but is currently suspected to be generated during the PCR enrichment process of the sequencing library preparation. Incomplete PCR amplification at this stage may lead to the formation of chimeras at priming sites, from fragments belonging to two different amplicons. The resulting fragment is therefore strictly identical to the genuine MOTU, but its tag combination (and hence sample origin) is an artifact. This bias is also frequency-dependent, i.e. abundant genuine MOTUs are more likely to be found in low abundance in samples were they are not supposed to be. The function aims at reducing the amount of such false positives, by considering each MOTU separately and reducing their abundance to 0 below a given \code{threshold} of in terms of proportion over the total MOTU abundance in the entire dataset. This reduction can be done with one of the two following methods:
+#'
+#' \itemize{
+#' \item{\code{method = "cut"} sets the MOTU abundance to 0.}
+#' \item{\code{method = "substract"} substracts the maximum MOTU abundance corresponding to
+#'       \code{threshold} to all abundances of that MOTU in the entire dataset.
+#'      Negative values obtained from this operation are set to 0}
+#' }
+#'
+#' @seealso \code{\link{contaslayer}}, \code{\link{pcrslayer}}
+#'          for other data curation procedures.
 #'
 #' @references Carlsen, T., Aas, A. B., Lindner, D., Vrålstad, T., Schumacher, T., & Kauserud, H. (2012). Don't make a mista (g) ke: is tag switching an overlooked source of error in amplicon pyrosequencing studies?. Fungal Ecology, 5(6), 747-749.
 #' @references Esling, P., Lejzerowicz, F., & Pawlowski, J. (2015). Accurate multiplexing and filtering for high-throughput amplicon-sequencing. Nucleic Acids Research, 43(5), 2513-2524.
-#'
 #' @references Schnell, I. B., Bohmann, K., & Gilbert, M. T. P. (2015). Tag jumps illuminated–reducing sequence‐to‐sample misidentifications in metabarcoding studies. Molecular Ecology Resources, 15(6), 1289-1303.
-#'
 #' @references Zinger, L., Taberlet, P., Schimann, H., Bonin, A., Boyer, F., De Barba, M., ... & Réjou‐Méchain, M. (2019). Body size determines soil community assembly in a tropical forest. Molecular Ecology, 28(3), 528-543.
-
+#'
 #' @examples
 #'
 #' data(soil_euk)
@@ -33,6 +44,7 @@
 #'     m$reads[, idx]
 #'   }
 #' )
+#'
 #' p1 + scale_size(limits = c(1, max(soil_euk$reads[, idx]))) +
 #'   ggtitle("Distribution of the most abundant MOTU")
 #'
@@ -49,9 +61,19 @@
 #' @author Lucie Zinger
 #' @export tagjumpslayer
 
-tagjumpslayer <- function(metabarlist, threshold = 0.03) {
+tagjumpslayer <- function(metabarlist, threshold = 0.03, method="cut") {
   if (suppressWarnings(check_metabarlist(metabarlist))) {
+
+    if(threshold<0 | threshold>1) {
+      stop("`threshold` should be between 0 and 1")
+    }
+
+    if(!method %in% c("cut", "substract")) {
+      stop("`method` should be `cut` or `substract`")
+    }
+
     reads_table <- metabarlist$reads
+
     for (y in 1:ncol(reads_table)) {
       cum <- cumsum(sort(reads_table[, y, drop = T], decreasing = T)) / sum(reads_table[, y])
       if (cum[1] >= (1 - threshold)) {
@@ -60,14 +82,26 @@ tagjumpslayer <- function(metabarlist, threshold = 0.03) {
         threshold2 <- threshold
       }
       out_tmp <- 1 - cum < threshold2 & # get OTUs in the threshold part of the distribution
-        c(cum[1], diff(cum)) < threshold2 # cum abund diff between OTUs necessarily above threshold
-      # cases if 1 occurrence (i.e. threshold2 = 1) => cum all FALSE but all other counts are null
-      # => single occurrence kept and counts unaffected because already ok
+        c(cum[1], diff(cum)) < threshold2 # cum abund diff between OTUs necessarily > threshold
+      # cases if 1 occurrence (i.e. threshold2 = 1)
+      #    => cum all FALSE but all other counts are null
+      #    => single occurrence kept and counts unaffected because already ok
       # cases if cum[1] > 1-threshold (above is a particular case)
-      # => occurrence with highest abundance kept. Rational is that if it is largely detected in one sample, then its occurrence elsewhere is most likely a tagjump.
+      #    => occurrence with highest abundance kept.
+      #    Rational is that if it is largely detected in one sample,
+      #    then its occurrence elsewhere is most likely a tagjump.
       # plot(cum, col=ifelse(out.tmp==F, "green", "red"), pch=19)
+
       out <- out_tmp[rownames(reads_table)]
-      metabarlist$reads[out == T, y] <- 0
+
+      if(method == "cut") {
+        metabarlist$reads[out == T, y] <- 0
+      } else {
+        val <- ifelse(all(out ==F), 0, max(metabarlist$reads[out == T, y]))
+        val.vec <- metabarlist$reads[, y] - val
+        metabarlist$reads[, y] <-
+          ifelse(val.vec < 0, 0, metabarlist$reads[, y] - val)
+      }
     }
     return(metabarlist)
   }
