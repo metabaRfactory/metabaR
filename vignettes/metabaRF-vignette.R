f4ffa69b9f66184843de354265dc36a333a20ebb
lzinger <lucie@zinger.fr>
2020-08-02 01:23:29 +0200

    Updated vignette

---
+++ vignettes/metabaRF-vignette.R
@@ -0,0 +1,16 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----install, eval=FALSE------------------------------------------------------
+#  install.packages("devtools")
+#  devtools::install_github("metabaRfactory/metabaRffe")
+
+## ----loadpackage--------------------------------------------------------------
+library(metabaRffe) # modify the name once we'll all agree on that
+
+## ----help---------------------------------------------------------------------
+?soil_euk
+
