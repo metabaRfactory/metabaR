185a98ee8c05fa5c90031611ff9bdb66a19903c4
metabaRfactory <metabarfactory@gmail.com>
2020-08-19 16:28:20 +0100

    tabfiles_to_metabarlist check

--- R/tabfiles_to_metabarlist.R
+++ R/tabfiles_to_metabarlist.R
@@ -10,14 +10,14 @@
 #'                      Rows of the table should correspond to MOTUs, and the columns to their
 #'                      characteristics. The first column of this table should contain MOTU names.
 #'                      Mandatory fields: `sequence`, i.e. the most abundant sequence of the MOTU.
-#' @param file_pcrs     path for the pcrs characteristics table (e.g. tags, primers, plate wells, etc.).
+#' @param file_pcrs     path for the PCR characteristics table (e.g. tags, primers, plate wells, etc.).
 #'                      Mandatory fields: (i) `sample_id`, i.e. the name of each sample. (ii) `type`,
 #'                      i.e. the type of pcr; can be `sample` or `control`. (iii) `control_type`,
 #'                      i.e. the type of control if applicable. Should be: `NA` for samples,
-#'                      `extraction` for extraction negative controls, `pcr` for pcr negative controls,
+#'                      `extraction` for extraction negative controls, `pcr` for PCR negative controls,
 #'                      `sequencing` for sequencing negative controls (e.g. unused tag combinations),
 #'                      and `positive` for positive controls. The first column of this table should
-#'                      correspond to the names of the pcrs.
+#'                      correspond to the names of the PCRs.
 #' @param file_samples  path for the sample characteristics table. The first column of this table
 #'                      should contain the sample names.
 #' @param files_sep     separator used to read the different tables. Should be the same in all tables.
@@ -30,7 +30,7 @@
 #'
 #' @details
 #'
-#' This function imports tabular data into \R to create a \code{\link{metabarlist}} object. The four files required are imported in \R, included into a list of class \code{\link{metabarlist}} with the \code{\link{metabarlist_generator}} function, and congruencies between all tables are tested with the \code{\link{check_metabarlist}} function.
+#' This function imports tabular data into \R to create a \code{\link{metabarlist}} object. The four files required are imported into \R, included in a list of class \code{\link{metabarlist}} with the \code{\link{metabarlist_generator}} function, and congruencies between all tables are tested with the \code{\link{check_metabarlist}} function.
 #'
 #' @examples
 #'
